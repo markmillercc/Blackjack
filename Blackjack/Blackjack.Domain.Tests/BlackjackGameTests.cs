7d54e87da7889842ea2ef38cde8104376de1dbaa
Mark Miller <markmiller885@gmail.com>
2017-03-16 21:44:19 -0700

    add domain tests project

---
+++ Blackjack/Blackjack.Domain.Tests/BlackjackGameTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blackjack.Domain.Tests
+{
+    [TestClass]
+    public class BlackjackGameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
