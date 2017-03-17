a63ca651c4f111ff57402838ca98539eea13919f
Mark Miller <markmiller885@gmail.com>
2017-03-16 21:59:23 -0700

    add domain test player and player account

---
+++ Blackjack/Blackjack.Domain.Tests/TestPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Domain.Tests
+{
+    class TestPlayer
+    {
+        public string Name { get; set; }
+        public TestPlayerAccount Account { get; set; }
+        public TestPlayer(string name)
+        {
+            Account = new TestPlayerAccount(10000);
+            Name = name;
+        }
+    }
+}
---
+++ Blackjack/Blackjack.Domain.Tests/TestPlayerAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Domain.Tests
+{
+    class TestPlayerAccount : IPlayerAccount
+    {
+        public string Id { get; private set; }
+        public double Balance { get; private set; }
+
+        public TestPlayerAccount(double startingBalance)
+        {
+            Id = Guid.NewGuid().ToString();
+            Balance = startingBalance;
+        }
+
+        public void Credit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Debit(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
